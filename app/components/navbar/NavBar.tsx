887eba06c24cb0d0377f6058eb9cb9835e81a3ae
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2025-07-18 19:34:27 -0300

    initial navbar

---
+++ app/components/navbar/NavBar.tsx
@@ -0,0 +1,14 @@
+import Container from "../container/Container";
+
+const NavBar =() => {
+    return (
+    <div className="fixed w-full bg-black z-10 shadow-sm">
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                Texto
+            </Container>
+        </div>
+    </div>
+    );
+}
+export default NavBar;
