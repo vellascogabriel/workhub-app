423f81fc665b58e80f776855606192558ee84d1d
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2025-07-18 21:59:52 -0300

    create logo component

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    weight: '900',
+    subsets: ['latin'],
+});
+
+const Logo = () => {
+    const router = useRouter();
+    
+    return (
+        <div 
+            onClick={() => router.push('/')}
+            className="cursor-pointer"
+        >
+            <div 
+                className={`${roboto.className} text-black text-[25px] flex items-baseline`}
+            >
+                Workhub <span className="text-[40px] text-[#006400] leading-none ml-[2px]">.</span>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;            
