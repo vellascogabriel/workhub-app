2815d0c3e36997559392cf66ffc2c1a85d612db6
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2025-07-21 23:55:00 -0300

    modal component wip (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "./components/navbar/NavBar";
-
+import Modal from "./components/modals/Modal";
 
 export const metadata: Metadata = {
   title: "Workhub",
@@ -17,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar />
+        <Modal  isOpen actionLabel="" />
         {children}
       </body>
     </html>
